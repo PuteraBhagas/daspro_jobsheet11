608201f677b2c0e325e72c41de40abddd81b0e21
PuteraBhagas <p.bhagaswara@gmail.com>
2023-12-08 07:26:25 +0700

    Percobaan 5 modifikasi

--- NestedLoop25.java
+++ NestedLoop25.java
@@ -7,20 +7,23 @@ public class NestedLoop25 {
         double temps [][] = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("City: " + i);
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print("Day " + (j + 1) + ": ");
-                temps[i][j] = scanner.nextDouble();
+            System.out.println("City: " + (i+1));
+            for (double temp : temps[i]) {
+                System.out.println(temp + " ");
             }
             System.out.println();
+         
         }
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("City: " + i);
+            System.out.println("City: " + (i+1));
+            double totaltemp=0;
             for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+               totaltemp += temps[i][j];
             }
-            System.out.println();
+           
+            double rata = totaltemp / temps[0].length;
+            System.out.println("Rata-rata temperatur kota : "+rata);
 
         }
     }
